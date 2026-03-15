2b31b877de28d5111e2aef409ed902461312c142
Maxmomaric029 <112379738+Maxmomaric029@users.noreply.github.com>
2026-03-15 07:57:07 -0400

    Update Android.mk

--- app/src/3/main/jni/Android.mk
+++ app/src/3/main/jni/Android.mk
@@ -1,63 +1,62 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdobby
 LOCAL_SRC_FILES := KhanhMods/Tools/Dobby/libraries/$(TARGET_ARCH_ABI)/libdobby.a
 LOCAL_EXPORT_C_INCLUDE := $(LOCAL_PATH)/KhanhMods/Tools/Dobby/
 include $(PREBUILT_STATIC_LIBRARY)
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := libcurl
 LOCAL_SRC_FILES := curl/curl-android-$(TARGET_ARCH_ABI)/lib/libcurl.a
 include $(PREBUILT_STATIC_LIBRARY)
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := libssl
 LOCAL_SRC_FILES := curl/openssl-android-$(TARGET_ARCH_ABI)/lib/libssl.a
 include $(PREBUILT_STATIC_LIBRARY)
 include $(CLEAR_VARS)
-
-# --------------------- Prebuilt: Munodz (kFox) --------------
-# Thư viện chứa implement cho kFox::SetSearchRange/MemorySearch/...
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmunodz
 LOCAL_SRC_FILES := Munodz/$(TARGET_ARCH_ABI)/libMunodz.a
-# Xuất đường dẫn header để module khác include được Munodz.hpp
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/Munodz
 include $(PREBUILT_STATIC_LIBRARY)
-
-
+include $(CLEAR_VARS)
 LOCAL_MODULE := libcrypto
 LOCAL_SRC_FILES := curl/openssl-android-$(TARGET_ARCH_ABI)/lib/libcrypto.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-
-MAIN_LOCAL_PATH := $(call my-dir)
-LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH)
-
 include $(CLEAR_VARS)
-
 LOCAL_MODULE    := anogs
 
-# Code optimization
 LOCAL_CFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w
 LOCAL_CFLAGS += -fno-rtti -fno-exceptions -fpermissive
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/KhanhMods/IL2CppSDK
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/KhanhMods
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/xdl
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/ImGui
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/ImGui/backends
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/Munodz
+
 LOCAL_CPPFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w -Werror -s -std=c++17
 LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fms-extensions -fno-rtti -fno-exceptions -fpermissive
+LOCAL_CPPFLAGS += -I$(LOCAL_PATH)/KhanhMods/IL2CppSDK
+LOCAL_CPPFLAGS += -I$(LOCAL_PATH)/KhanhMods
+LOCAL_CPPFLAGS += -I$(LOCAL_PATH)/xdl
+LOCAL_CPPFLAGS += -I$(LOCAL_PATH)/ImGui
+LOCAL_CPPFLAGS += -I$(LOCAL_PATH)/ImGui/backends
+LOCAL_CPPFLAGS += -I$(LOCAL_PATH)/Munodz
+
 LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all, -llog
 LOCAL_ARM_MODE := arm
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv3 -lGLESv2 -lGLESv1_CM -lz
 
-LOCAL_C_INCLUDE := $(LOCAL_PATH)
-LOCAL_C_INCLUDE := $(LOCAL_PATH)/xdl
-LOCAL_C_INCLUDE := $(LOCAL_PATH)/ImGui
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/Munodz
-LOCAL_C_INCLUDE := $(LOCAL_PATH)/ImGui/backends
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/KhanhMods
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/KhanhMods/IL2CppSDK
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/xdl
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/ImGui
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/ImGui/backends
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/Munodz
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/curl/curl-android-$(TARGET_ARCH_ABI)/include
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/curl/openssl-android-$(TARGET_ARCH_ABI)/include
-
 
 LOCAL_SRC_FILES := Main.cpp \
 xdl/xdl.c \
@@ -88,8 +87,6 @@ KhanhMods/Tools/MonoString.cpp \
 KhanhMods/Tools/Tools.cpp \
 oxorany/oxorany.cpp \
 
-
 LOCAL_STATIC_LIBRARIES := libdobby libcurl libssl libcrypto libmunodz
 LOCAL_CPP_FEATURES     := exceptions
 include $(BUILD_SHARED_LIBRARY)
-
